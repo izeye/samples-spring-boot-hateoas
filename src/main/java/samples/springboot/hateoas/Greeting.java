a0df72144d86e6364122218f2d72c605dc2df005
izeye <izeye@naver.com>
2014-11-12 22:47:05 +0900

    Add a sample from http://spring.io/guides/gs/rest-hateoas/.

---
+++ src/main/java/samples/springboot/hateoas/Greeting.java
@@ -0,0 +1,23 @@
+package samples.springboot.hateoas;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * Created by izeye on 2014. 11. 12..
+ */
+public class Greeting extends ResourceSupport {
+
+    private final String content;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
